44f3368628326d789efa6d9f9c75595cef24e971
Ricky-Thomas <rickythomas.rt@gmail.com>
2015-01-07 09:05:51 -0500

    Initial skeleton

---
+++ app/controllers/auth.rb
@@ -0,0 +1,36 @@
+get '/login' do
+  redirect '/'
+end
+
+post '/login' do
+  user = User.find_by(email: params[:user][:email])
+  if user && user.authenticate(params[:user][:password])
+    session[:user_id] = user.id
+    redirect("/")
+  else
+    set_error('login failed.')
+    redirect('/login')
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect('/')
+end
+
+get '/signup' do
+  redirect '/'
+end
+
+post '/signup' do
+  new_user = User.new(params[:user])
+
+  if new_user.valid? && params[:user][:password] == params[:password_confirm]
+    new_user.save
+    session[:user_id] = new_user.id
+    redirect("/")
+  else
+    set_error('signup failed.')
+    redirect('/')
+  end
+end
