bb3202b33b2c59e653947a68724153132a6cf29d
Ricky Thomas <rickythomas.rt@gmail.com>
2015-01-07 09:20:30 -0500

    Add API module with more specific search method

---
+++ app/helpers/api.rb
@@ -0,0 +1,10 @@
+module API
+
+  def self.char_search(query)
+    list = ComicVine::API.search('character', query)
+    list = list.find_all {|i| i.name.downcase == query}
+    sorted_list = list.sort_by {|i| i.count_of_issue_appearances}
+    return sorted_list.last
+  end
+
+end
