185131333e1289c629f7e0677c5d7326edd58596
Ricky Thomas <rickythomas.rt@gmail.com>
2015-01-07 09:47:33 -0500

    Join table migrations

--- db/migrate/20150107092534_create_volume_shelves.rb
+++ db/migrate/20150107092534_create_volume_shelves.rb
@@ -1,4 +1,9 @@
 class CreateVolumeShelves < ActiveRecord::Migration
   def change
+    create_table :volume_shelves do |t|
+      t.references :user
+      t.references :volume
+      t.string     :comment
+      t.timestamps
   end
 end
--- db/migrate/20150107092542_create_issue_shelves.rb
+++ db/migrate/20150107092542_create_issue_shelves.rb
@@ -1,4 +1,9 @@
 class CreateIssueShelves < ActiveRecord::Migration
   def change
+    create_table :issue_shelves do |t|
+      t.references :user
+      t.references :issue
+      t.string     :comment
+      t.timestamps
   end
 end
