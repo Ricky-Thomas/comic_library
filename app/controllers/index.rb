83372b2ff84d3fb3fdbf3d5060c7fa1d41428f37
Ricky Thomas <rickythomas.rt@gmail.com>
2015-01-07 11:46:59 -0500

    Psuedocoding routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,4 +2,20 @@ get '/' do
 
 end
 
+get '/volumes'
+end
 
+get '/volumes/:id' do |id|
+end
+
+get '/issues' do |id|
+end
+
+get '/issues/:id' do |id|
+end
+
+get '/volume_shelf/:user_id' do |user_id|
+end
+
+get '/issue_shelf/:user_id' do |user_id|
+end
